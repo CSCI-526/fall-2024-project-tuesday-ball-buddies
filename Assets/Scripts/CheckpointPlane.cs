33d59388ecb39810739a30809eb41dd2fd6ab506
leechakyin <leechakhim@gmail.com>
2024-10-14 08:19:18 -0700

    Update CheckpointPlane.cs

--- Assets/Scripts/CheckpointPlane.cs
+++ Assets/Scripts/CheckpointPlane.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class CheckpointPlane : MonoBehaviour
 {
+    private Renderer checkpointRenderer;
+
+    void Start()
+    {
+        checkpointRenderer = GetComponent<Renderer>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         BallControl ball = other.GetComponent<BallControl>();
         if (ball != null)
         {
+            // Set the checkpoint for the ball
             ball.SetCheckpoint(transform.position);
             Debug.Log("Checkpoint set at: " + transform.position);
+
+            // Change the color of the checkpoint to green
+            ChangeCheckpointColor(Color.green);
+        }
+    }
+
+    // Method to change the color of the checkpoint plane
+    private void ChangeCheckpointColor(Color color)
+    {
+        if (checkpointRenderer != null)
+        {
+            checkpointRenderer.material.color = color;
+        }
+        else
+        {
+            Debug.LogWarning("No Renderer found on the CheckpointPlane!");
         }
     }
 }
