342151afeb0e9db556c5d2af46f45c97d777a784
chr8stoph <57600205+cbathon@users.noreply.github.com>
2024-10-17 22:10:59 -0700

    fixed camera angle

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 30, -10);
+    private Vector3 offset = new Vector3(0, 25, -20);
     public Canvas hudCanvas;
     public HUDManager hudManager;
 
