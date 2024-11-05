3831d2d51acd98f54a77998e6f357bbb5860d7f6
xrr-233 <rxu17988@usc.edu>
2024-11-05 14:46:45 -0800

    add emission

--- Assets/Scripts/FloatingObject.cs
+++ Assets/Scripts/FloatingObject.cs
@@ -2,17 +2,17 @@ using UnityEngine;
 
 public class FloatingObject : MonoBehaviour
 {
-    public float floatStrength = 1f;  
-    public float floatSpeed = 1f;     
-    private Vector3 startPos;
+    public float floatStrength = 1f;
+    public float floatSpeed = 1f;
+    private Vector3 startLocalPos;
 
     void Start()
     {
-        startPos = transform.position;
+        startLocalPos = transform.localPosition;
     }
 
     void Update()
     {
-        transform.position = startPos + new Vector3(0, Mathf.Sin(Time.time * floatSpeed) * floatStrength, 0);
+        transform.localPosition = startLocalPos + new Vector3(0, Mathf.Sin(Time.time * floatSpeed) * floatStrength, 0);
     }
 }
